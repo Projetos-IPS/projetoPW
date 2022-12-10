9bf0600288e2a05cfa506af182b17299cdf06eaa
Joana Silva <101148531+JoanaSilvaHUS@users.noreply.github.com>
2022-12-10 14:41:36 +0000

    botao login adicionado

--- projeto/www/scripts/joboffers.js
+++ projeto/www/scripts/joboffers.js
@@ -12,7 +12,7 @@ const jobOfferPosition = {
 
 
 var data = [
-    { companyName: 'Kwalee', description: 'Senior Software Engineer - Frontend', position: jobOfferPosition.SOFTWAREENGINEER, duration: 'Fulltime', location: 'Lisbon/Portugal', salary: '€39,532',  uptime:'5'}, //days
+    { companyName: 'Kwalee', description: 'Senior Software Engineer - Frontend', position: jobOfferPosition.SOFTWAREENGINEER, duration: 'Fulltime', location: 'Lisbon/Portugal', salary: '€39,532',  uptime:'5'}, 
     { companyName: 'Reaktor', description: 'Senior Frontend Engineer', position: jobOfferPosition.SOFTWAREENGINEER, duration: 'Fulltime', location: 'Lisbon/Portugal', salary: '€42,000',  uptime:'5'},
     { companyName: 'Smart Consulting', description: 'Java developer Junior', position: jobOfferPosition.COMPUTERPROGRAMMER, duration: 'Fulltime', location: 'Lisbon/Portugal', salary: '€42,000',  uptime:'5'},
     { companyName: 'DevoTeam', description: 'Data Analyst', position: jobOfferPosition.DATASCIENTIST, duration: 'Fulltime/Remote', location: 'Lisbon/Portugal', salary: '€52,000',  uptime:'5'},
