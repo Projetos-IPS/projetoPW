11a53799fab7bb89a1a527e61f518afead382846
Joana Silva <101148531+JoanaSilvaHUS@users.noreply.github.com>
2022-12-10 01:39:21 +0000

    Update joboffers.js

--- projeto/www/scripts/joboffers.js
+++ projeto/www/scripts/joboffers.js
@@ -15,8 +15,8 @@ var data = [
     { companyName: 'Kwalee', description: 'Senior Software Engineer - Frontend', position: jobOfferPosition.SOFTWAREENGINEER, duration: 'Fulltime', location: 'Lisbon/Portugal', salary: '€39,532',  uptime:'5'}, //days
     { companyName: 'Reaktor', description: 'Senior Frontend Engineer', position: jobOfferPosition.SOFTWAREENGINEER, duration: 'Fulltime', location: 'Lisbon/Portugal', salary: '€42,000',  uptime:'5'},
     { companyName: 'Smart Consulting', description: 'Java developer Junior', position: jobOfferPosition.COMPUTERPROGRAMMER, duration: 'Fulltime', location: 'Lisbon/Portugal', salary: '€42,000',  uptime:'5'},
-    { companyName: 'Smart Consulting', description: 'Java developer Junior', position: jobOfferPosition.COMPUTERPROGRAMMER, duration: 'Fulltime', location: 'Lisbon/Portugal', salary: '€42,000',  uptime:'5'},
-   { companyName: 'Smart Consulting', description: 'Java developer Junior', position: jobOfferPosition.COMPUTERPROGRAMMER, duration: 'Fulltime', location: 'Lisbon/Portugal', salary: '€42,000',  uptime:'5'},
+    { companyName: 'DevoTeam', description: 'Data Analyst', position: jobOfferPosition.DATASCIENTIST, duration: 'Fulltime/Remote', location: 'Lisbon/Portugal', salary: '€52,000',  uptime:'5'},
+   { companyName: 'ADENTIS Portugal', description: 'SQL developer', position: jobOfferPosition.DATABASEADMINISTRATOR, duration: 'Fulltime/Remote', location: 'Lisbon/Portugal', salary: '€47,000',  uptime:'5'},
     
     
      ];
