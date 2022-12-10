de68232d4f2f67c9a7521f9bb15637df83c58790
Joana Silva <101148531+JoanaSilvaHUS@users.noreply.github.com>
2022-12-10 01:25:44 +0000

    job offers page

---
+++ projeto/www/scripts/joboffers.js
@@ -0,0 +1,25 @@
+
+const jobOfferPosition = {
+    SUPPORTSPECIALIST: "Support specialist",
+    WEBDEVELOPER: "Web developer",
+    COMPUTERPROGRAMMER: "Computer programmer",
+    SYSTEMSANALYST: "Systems analyst",
+    SOFTWAREENGINEER: "Software engineer",
+    DATABASEADMINISTRATOR: "Database administrator",
+    DATASCIENTIST: "Data scientist",
+    ITDIRECTOR: "IT director"
+}
+
+
+var data = [
+    { companyName: 'Kwalee', description: 'Senior Software Engineer - Frontend', position: jobOfferPosition.SOFTWAREENGINEER, duration: 'Fulltime', location: 'Lisbon/Portugal', salary: '€39,532',  uptime:'5'}, //days
+    { companyName: 'Reaktor', description: 'Senior Frontend Engineer', position: jobOfferPosition.SOFTWAREENGINEER, duration: 'Fulltime', location: 'Lisbon/Portugal', salary: '€42,000',  uptime:'5'},
+    { companyName: 'Smart Consulting', description: 'Java developer Junior', position: jobOfferPosition.COMPUTERPROGRAMMER, duration: 'Fulltime', location: 'Lisbon/Portugal', salary: '€42,000',  uptime:'5'},
+    { companyName: 'Smart Consulting', description: 'Java developer Junior', position: jobOfferPosition.COMPUTERPROGRAMMER, duration: 'Fulltime', location: 'Lisbon/Portugal', salary: '€42,000',  uptime:'5'},
+   { companyName: 'Smart Consulting', description: 'Java developer Junior', position: jobOfferPosition.COMPUTERPROGRAMMER, duration: 'Fulltime', location: 'Lisbon/Portugal', salary: '€42,000',  uptime:'5'},
+    
+    
+     ];
+
+
+
