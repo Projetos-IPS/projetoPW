bf025cd28ee755468684b46d788b2383242b419d
Joana Silva <101148531+JoanaSilvaHUS@users.noreply.github.com>
2023-01-10 20:32:16 +0000

    SERVIDOR FEITO COM PUG, EXPRESS - NODE.JS

--- 1F_PW_202100190_202100242/www/app.js
+++ 1F_PW_202100190_202100242/www/app.js
@@ -1,18 +1,34 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const requestHandlers = require("./request-handlers");
-
-
 const app = express();
-app.use(bodyParser.urlencoded());
+//const bodyParser = require("body-parser");
+//const requestHandlers = require("./request-handlers.js");
 
-app.use(express.static("views", { 
-    
-    "index": "index.html",
-    "joboffers": "joboffers.html",
-    "team": "team.html"
+const port = 8081;
+const host = 'localhost';
 
-}));
+app.set('view engine', 'pug');
+app.use(express.static('public'));
 
-app.listen(8081, function() {
-    console.log("Server running at http://localhost:8081");});
+
+app.get('/', (req, res) => {
+	res.render('index.pug');
+});
+
+app.get('/Job%20offers', (req, res) => {
+	res.render('joboffers.pug');
+});
+
+app.get('/Homepage', (req, res) => {
+	res.render('index.pug');
+});
+
+app.get('/Team', (req, res) => {
+	res.render('team.pug');
+});
+
+
+app.listen(port, host, () => {
+	console.log(`Server running at http://${host}:${port}`);
+});
+
+
