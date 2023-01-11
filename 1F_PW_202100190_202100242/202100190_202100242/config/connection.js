a59104ef5c6e9ac56ac9fd5b96a059ff9c7edcb2
Joana Silva <101148531+JoanaSilvaHUS@users.noreply.github.com>
2023-01-11 22:23:10 +0000

    Update connection.js

---
+++ 1F_PW_202100190_202100242/202100190_202100242/config/connection.js
@@ -0,0 +1,8 @@
+const mysql = require("mysql");
+
+const connectionOptions = {
+    host : "localhost",
+    user: "root",
+    password: "12345678",
+    database: "proj_bd_202100190_202100242"
+};
