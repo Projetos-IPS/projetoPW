a7a3ea1d5b3be6a695eb8abafc6041d5ce36cbf2
Joana Silva <101148531+JoanaSilvaHUS@users.noreply.github.com>
2023-01-13 16:27:39 +0000

    Delete login.js

--- 1F_PW_202100190_202100242/202100190_202100242/routes/login.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var routerJ = express.Router();
-var path = require('path');
-
-routerJ.get('/', function (req, res) {
-	res.render(path.join(__dirname + "/../views/login.pug"));
-});
-
-module.exports = routerJ;
