10b63af445fc55e8d7684ceb9fcdde5d6503f1e1
Joana Silva <101148531+JoanaSilvaHUS@users.noreply.github.com>
2023-01-13 22:04:33 +0000

    Update connection.js

--- 1F_PW_202100190_202100242/202100190_202100242/config/connection.js
+++ 1F_PW_202100190_202100242/202100190_202100242/config/connection.js
@@ -1,14 +1,22 @@
-const mysql = require("mysql");
-
-const connectionOptions = {
+var config = {
+    database: { 
     host : "localhost",
     user: "root",
     password: "12345678",
+    port: 3306,
     database: "proj_bd_202100190_202100242"
-};
+    },
+    server: {
+      host: '127.0.01',
+      port: '8888'
+    }
+}
 
+module.exports = config;
+
+/*
 connection.connect(function(err) {
     if (err) throw err
     console.log('Está conectado à BD ...')
   })
-  //fim da conexão MySQL.
+  //fim da conexão MySQL.*/
