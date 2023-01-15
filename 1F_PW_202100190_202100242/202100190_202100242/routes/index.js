9d6d78c54f6d9b6072d62fdfc71f588d8b8f06f5
Joana Silva <101148531+JoanaSilvaHUS@users.noreply.github.com>
2023-01-15 13:39:30 +0000

    registo de empresas e profissionais feito (sem validações)

--- 1F_PW_202100190_202100242/202100190_202100242/routes/index.js
+++ 1F_PW_202100190_202100242/202100190_202100242/routes/index.js
@@ -12,34 +12,6 @@ router.get('/', function(req, res, next) {
 
 router.post('/registoP', function(request, response, next){
 
-   var action = request.body.action;
-
-  if(action == 'AddP')
-  {
-    var nome = request.body.nomeP;
-    var email = request.body.emailP;
-    var pass = request.body.passP;
-    var birthday = request.body.birthdayP;
-    var gender = request.body.genderP;
-    var query = `INSERT INTO profissional (email,nome, pass, data_nascimento, genero, tipo_utilizador)
-    values ("${email}", "${nome}", "${pass}", "${birthday}", "${gender}", "profissional" )`;
-    database.query(query, function(error, data){
-
-			response.json({
-				message : 'Data Added'
-			});
-
-		});
-
-  }
-});
-
-router.post('/registoP', function(request, response, next){
-
-  var action = request.body.action;
-
- if(action == 'AddP')
- {
    var nome = request.body.nomeP;
    var email = request.body.emailP;
    var pass = request.body.passP;
@@ -54,16 +26,10 @@ router.post('/registoP', function(request, response, next){
      });
 
    });
-
- }
 });
 
 router.post('/registoE', function(request, response, next){
 
-  var action = request.body.action;
-
- if(action == 'AddE')
- {
    var nome = request.body.nomeE;
    var email = request.body.emailE;
    var pass = request.body.passE;
@@ -76,7 +42,6 @@ router.post('/registoE', function(request, response, next){
      });
 
    });
- }
 });
 
 
