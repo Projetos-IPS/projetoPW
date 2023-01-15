d6fbd1d7894df204b1ebde1cac8f5523698a69de
Joana Silva <101148531+JoanaSilvaHUS@users.noreply.github.com>
2023-01-15 01:12:19 +0000

    yes

--- 1F_PW_202100190_202100242/202100190_202100242/routes/index.js
+++ 1F_PW_202100190_202100242/202100190_202100242/routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.post('/action', function(request, response, next){
+router.post('/registoP', function(request, response, next){
 
    var action = request.body.action;
 
@@ -32,8 +32,51 @@ router.post('/action', function(request, response, next){
 		});
 
   }
+});
 
+router.post('/registoP', function(request, response, next){
 
+  var action = request.body.action;
+
+ if(action == 'AddP')
+ {
+   var nome = request.body.nomeP;
+   var email = request.body.emailP;
+   var pass = request.body.passP;
+   var birthday = request.body.birthdayP;
+   var gender = request.body.genderP;
+   var query = `INSERT INTO profissional (email,nome, pass, data_nascimento, genero, tipo_utilizador)
+   values ("${email}", "${nome}", "${pass}", "${birthday}", "${gender}", "profissional" )`;
+   database.query(query, function(error, data){
+
+     response.json({
+       message : 'Data Added'
+     });
+
+   });
+
+ }
+});
+
+router.post('/registoE', function(request, response, next){
+
+  var action = request.body.action;
+
+ if(action == 'AddE')
+ {
+   var nome = request.body.nomeE;
+   var email = request.body.emailE;
+   var pass = request.body.passE;
+   var query = `INSERT INTO empresa (email, nome, pass, tipo_utilizador)
+   values ("${email}", "${nome}", "${pass}", "empresa" )`;
+   database.query(query, function(error, data){
+
+     response.json({
+       message : 'Data Added'
+     });
+
+   });
+ }
 });
 
 
