3a6f3149fdf90381d5de328e5c8eccfd2c87c730
Joana Silva <101148531+JoanaSilvaHUS@users.noreply.github.com>
2023-01-15 19:56:05 +0000

    Squashed commit of the following:
    commit bc3b3a2e13672a918df3855d8a7f9d6360590b11
    Author: Joana Silva <101148531+JoanaSilvaHUS@users.noreply.github.com>
    Date:   Sun Jan 15 19:54:09 2023 +0000
    
        registo de utilizadores sem ajax e validações feito

--- 1F_PW_202100190_202100242/202100190_202100242/routes/index.js
+++ 1F_PW_202100190_202100242/202100190_202100242/routes/index.js
@@ -17,32 +17,28 @@ router.post('/registoP', function(request, response, next){
    var pass = request.body.passP;
    var birthday = request.body.birthdayP;
    var gender = request.body.genderP;
-   var query = `INSERT INTO profissional (email,nome, pass, data_nascimento, genero, tipo_utilizador)
-   values ("${email}", "${nome}", "${pass}", "${birthday}", "${gender}", "profissional" )`;
-   database.query(query, function(error, data){
-
-     response.json({
-       message : 'Data Added'
-     });
-
-   });
+   var query = `INSERT INTO profissional (email,nome, pass, data_nascimento, genero)
+   values ("${email}", "${nome}", "${pass}", "${birthday}", "${gender}")`;
+   database.query(query);
+   var query2 = `INSERT INTO utilizador(email, tipo_utilizador) values ("${email}", "profissional")`;
+   database.query(query2);
+   response.redirect('http://localhost:8081/');
 });
+
 
 router.post('/registoE', function(request, response, next){
 
    var nome = request.body.nomeE;
    var email = request.body.emailE;
    var pass = request.body.passE;
-   var query = `INSERT INTO empresa (email, nome, pass, tipo_utilizador)
-   values ("${email}", "${nome}", "${pass}", "empresa" )`;
-   database.query(query, function(error, data){
-
-     response.json({
-       message : 'Data Added'
-     });
-
-   });
+   var query = `INSERT INTO empresa (email, nome, pass)
+   values ("${email}", "${nome}", "${pass}")`;
+   database.query(query);
+   var query2 = `INSERT INTO utilizador(email, tipo_utilizador) values ("${email}", "empresa")`;
+   database.query(query2);
+   response.redirect('http://localhost:8081/');
 });
+
 
 
 module.exports = router;
