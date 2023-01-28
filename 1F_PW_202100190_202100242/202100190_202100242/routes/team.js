e8a4a981ffa7f78a20ba76de75b027119058289a
Joana Silva <101148531+JoanaSilvaHUS@users.noreply.github.com>
2023-01-28 15:15:10 +0000

    ay

--- 1F_PW_202100190_202100242/202100190_202100242/routes/team.js
+++ 1F_PW_202100190_202100242/202100190_202100242/routes/team.js
@@ -1,9 +1,52 @@
 var express = require('express');
+const { name } = require('ejs');
 var router = express.Router();
+var db = require('../config/connection');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('team', { title: 'Team' });
+  var email = req.session.name;
+var query = `SELECT tipo_utilizador FROM utilizador WHERE email = "${email}"`;
+db.query(query, function(error,data){
+
+  if (data.length > 0)
+  {
+    for (var count = 0; count < data.length; count++)
+    {
+      if(data[count].tipo_utilizador === "profissional")
+      {
+        var query2 = `SELECT nome FROM profissional WHERE email = "${email}"`;
+        db.query(query2, function(error,data)
+        {
+            if(data.length > 0)
+            {
+              for(var count = 0; count < data.length; count++)
+              {
+                var nameU = data[count].nome;
+              }
+            }
+            res.render('team', { title: 'Team', name: nameU});
+        });
+      }
+      if(data[count].tipo_utilizador == 'empresa')
+      {
+        var query2 = `SELECT nome FROM empresa WHERE email = "${email}"`;
+        db.query(query2, function(error,data)
+        {
+            if(data.length > 0)
+            {
+              for(var count = 0; count < data.length; count++)
+              {
+                var nameU = data[count].nome;
+              }
+            }
+            res.render('team', { title: 'Team', name: nameU});
+        });
+      }       
+     }
+  }
+});
+ 
 });
 
 module.exports = router;
