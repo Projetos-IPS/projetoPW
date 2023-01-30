04e016c2ccdca3dcc527ea9a757d8abf8458107b
Joana Silva <101148531+JoanaSilvaHUS@users.noreply.github.com>
2023-01-30 19:29:21 +0000

    pq que isto funciona??

--- 1F_PW_202100190_202100242/202100190_202100242/routes/team.js
+++ 1F_PW_202100190_202100242/202100190_202100242/routes/team.js
@@ -2,45 +2,10 @@ var express = require('express');
 const { name } = require('ejs');
 var router = express.Router();
 var db = require('../config/connection');
+var getUser = require('../models/getUser');
 
 /* GET home page. */
 router.get('/', getUser, renderPage);
-
-function getUser(req, res, next) {
-
-  var email = req.session.name;
-  if(email !== undefined)
-  {
-  query = `SELECT tipo_utilizador FROM utilizador WHERE EMAIL = "${email}"`;
-  db.query(query, function(err, result)
-  {
-    var user = result[0].tipo_utilizador;
-    if(user == 'profissional')
-    {
-      query2 = `SELECT nome FROM profissional where email = "${email}"`;
-      db.query(query2, function(err, result)
-      {
-        res.locals.name = result[0].nome;
-        next();
-      });
-    }
-    else if(user == 'empresa')
-    {
-      query2 = `SELECT nome FROM empresa where email = "${email}"`;
-      db.query(query2, function(err, result)
-      {
-        res.locals.name = result[0].nome;
-        next();
-      });
-    }
-  });
-}
-else
-{
-  res.redirect('Homepage');
-  res.end();
-}
-};
 
  function renderPage(req, res)
  {
