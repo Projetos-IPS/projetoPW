1a7216df3a02f51c50e25e1b0c8e7e9d933b9611
Joana Silva <101148531+JoanaSilvaHUS@users.noreply.github.com>
2023-02-04 23:42:13 +0000

    Update index.js

--- 1F_PW_202100190_202100242/202100190_202100242/routes/index.js
+++ 1F_PW_202100190_202100242/202100190_202100242/routes/index.js
@@ -8,10 +8,14 @@ router.get('/', function(req, res, next) {
 
   if(req.session.name !== undefined && req.session.name !== 0 && req.session.name !== 2 && req.session.name !== 3)
   {
-    req.session.destroy;
+    req.session.destroy((err) => {
+      res.render('index');
+    })
   }
- 
+  else
+  {
   res.render('index');
+  }
   
 });
 
