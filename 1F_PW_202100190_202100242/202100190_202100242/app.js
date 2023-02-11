2aa19309b1e337aa99163e66c65157a1d88ec782
Laura Costa <202100242@estudantes.ips.pt>
2023-02-11 22:15:03 +0000

    changes

--- 1F_PW_202100190_202100242/202100190_202100242/app.js
+++ 1F_PW_202100190_202100242/202100190_202100242/app.js
@@ -49,8 +49,6 @@ app.use('/Profile', profileRouter);
 app.use('/Home', homeRouter);
 app.use('/Approve', approveRouter);
 
-
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
