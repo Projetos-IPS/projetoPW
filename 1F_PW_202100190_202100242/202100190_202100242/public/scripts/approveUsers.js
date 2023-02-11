1cfca963d6c47fd4193630b8aa58bbb70278e0ef
Laura Costa <202100242@estudantes.ips.pt>
2023-02-11 21:57:11 +0000

    some changes on approve page

--- 1F_PW_202100190_202100242/202100190_202100242/public/scripts/approveUsers.js
+++ 1F_PW_202100190_202100242/202100190_202100242/public/scripts/approveUsers.js
@@ -1,203 +1,164 @@
-function getUsers(){
+function getUsers() {
     const xhrUsers = new XMLHttpRequest();
     xhrUsers.open('GET', '/Approve/getUsers', true);
     xhrUsers.setRequestHeader('Content-Type', 'application/json');
     var users_table = document.getElementById('users-data').getElementsByTagName('tbody')[0];
 
-    xhrUsers.onload = function(){
-        
-        if(xhrUsers.status === 200){
+    xhrUsers.onload = function() {
+        if(xhrUsers.status === 200) {
             var dataUser = JSON.parse(xhrUsers.responseText);
 
-            if(dataUser.length > 0)
-            {
-            for(var count = 0; count < dataUser.length; count++)
-            {
-            var btnEdit = document.createElement('button');
-            var btnDeactivate = document.createElement('button');
-            btnDeactivate.innerText = "Deactivate";
-            btnEdit.innerText = "Approve";
-            btnEdit.className += "editBtn";
-            btnEdit.id += "editBtn";
-            var btnDelete = document.createElement('button');
-            btnDelete.className += "deleteBtn";
-            btnDelete.innerText = "Reject";
-            var row = document.createElement("tr");
-            var c = document.createElement("td");   
+            if(dataUser.length > 0) {
+                for(var count = 0; count < dataUser.length; count++) {
+                    var btnEdit = document.createElement('button');
+                    var btnDeactivate = document.createElement('button');
 
-            c.innerText = dataUser[count].id;
-            row.appendChild(c);
-            users_table.appendChild(row);
+                    btnDeactivate.innerText = "Deactivate";
+                    btnEdit.innerText = "Approve";
+                    btnEdit.className += "editBtn";
+                    btnEdit.id += "editBtn";
+
+                    var btnDelete = document.createElement('button');
+
+                    btnDelete.className += "deleteBtn";
+                    btnDelete.innerText = "Reject";
+
+                    var row = document.createElement("tr");
+                    var c = document.createElement("td");   
+
+                    c.innerText = dataUser[count].id;
+                    row.appendChild(c);
+                    users_table.appendChild(row);
             
-            var c = document.createElement("td");   
-            c.innerText = dataUser[count].nome;
-            row.appendChild(c);
+                    var c = document.createElement("td");   
+                    c.innerText = dataUser[count].nome;
+                    row.appendChild(c);
 
-            users_table.appendChild(row);
-            var c = document.createElement("td");   
-            c.innerText = dataUser[count].email;
-            row.appendChild(c);
+                    users_table.appendChild(row);
+                    var c = document.createElement("td");   
+                    c.innerText = dataUser[count].email;
+                    row.appendChild(c);
 
-            users_table.appendChild(row);
-            var c = document.createElement("td");   
-            if(dataUser[count].tipo_utilizador == "Empresa")
-            {
-                c.innerText = 'Company';
-            }
-            row.appendChild(c);
-            users_table.appendChild(row);
+                    users_table.appendChild(row);
+                    var c = document.createElement("td"); 
 
-            var c = document.createElement("td");   
-            if(dataUser[count].approved == 0)
-            {
-                c.innerText = 'Not approved';
-                row.appendChild(c);
-                users_table.appendChild(row);
+                    if(dataUser[count].tipo_utilizador == "Empresa") {
+                        c.innerText = 'Company';
+                    }
+
+                    row.appendChild(c);
+                    users_table.appendChild(row);
+                    var c = document.createElement("td");   
+
+                    if(dataUser[count].approved == 0) {
+                        c.innerText = 'Not approved';
+                        row.appendChild(c);
+                        users_table.appendChild(row);
     
-                
-                btnEdit.dataset.userid = dataUser[count].email;
-                btnDelete.dataset.userid = dataUser[count].email;
-                btnDeactivate.style.display = 'none';
-            } 
-            if(dataUser[count].approved == 1)
-            {
-                c.innerText = 'Approved';
-                row.appendChild(c);
-                btnEdit.style.display = 'none';
-                btnDelete.style.display = 'none';
-                btnDeactivate.dataset.userid = dataUser[count].email;
-            }
-          
+                        btnEdit.dataset.userid = dataUser[count].email;
+                        btnDelete.dataset.userid = dataUser[count].email;
+                        btnDeactivate.style.display = 'none';
+                    } 
+
+                    if(dataUser[count].approved == 1) {
+                        c.innerText = 'Approved';
+                        row.appendChild(c);
+                        btnEdit.style.display = 'none';
+                        btnDelete.style.display = 'none';
+                        btnDeactivate.dataset.userid = dataUser[count].email;
+                    }
             
-   //----------------------------------------------------------
-   btnEdit.addEventListener("click", function(event){
+                    //----------------------------------------------------------
+                    btnEdit.addEventListener("click", function(event) {
+                        const clickedButton = event.target;
+                        var data = {
+                            userid : clickedButton.dataset.userid
+                        }
 
-    const clickedButton = event.target;
-    var data = {
-        userid : clickedButton.dataset.userid
-     }
-    const xhrUpdate = new XMLHttpRequest();
-    xhrUpdate.open('POST', '/Approve/UpdateUser', true);
-    xhrUpdate.setRequestHeader('Content-Type', 'application/json');
-    xhrUpdate.send(JSON.stringify(data));
+                        const xhrUpdate = new XMLHttpRequest();
+                        xhrUpdate.open('POST', '/Approve/UpdateUser', true);
+                        xhrUpdate.setRequestHeader('Content-Type', 'application/json');
+                        xhrUpdate.send(JSON.stringify(data));
    
-     xhrUpdate.onload = function(){
-        if(xhrUpdate.status === 200)
-      {
-        var response = JSON.parse(xhrUpdate.responseText);
+                        xhrUpdate.onload = function() {
+                            if(xhrUpdate.status === 200) {
+                                var response = JSON.parse(xhrUpdate.responseText);
 
-        if(response == 0)
-        {
-            users_table.innerHTML="";
-            getUsers();
-        }
-    }
-}
-   })
-//----------------------------------------------------------
-   //----------------------------------------------------------
-   btnDelete.addEventListener("click", function(event){
+                                if(response == 0) {
+                                    users_table.innerHTML="";
+                                    getUsers();
+                                }
+                            }
+                        }
+                    })
+                    //----------------------------------------------------------
 
-    const clickedButton = event.target;
-    var data = {
-        userid : clickedButton.dataset.userid
-     }
-    const xhrUpdate = new XMLHttpRequest();
-    xhrUpdate.open('POST', '/Approve/RejectUser', true);
-    xhrUpdate.setRequestHeader('Content-Type', 'application/json');
-    xhrUpdate.send(JSON.stringify(data));
+                    //----------------------------------------------------------
+                    btnDelete.addEventListener("click", function(event) {
+                        const clickedButton = event.target;
+                        var data = {
+                            userid : clickedButton.dataset.userid
+                        }
+
+                        const xhrUpdate = new XMLHttpRequest();
+                        xhrUpdate.open('POST', '/Approve/RejectUser', true);
+                        xhrUpdate.setRequestHeader('Content-Type', 'application/json');
+                        xhrUpdate.send(JSON.stringify(data));
    
-     xhrUpdate.onload = function(){
-        if(xhrUpdate.status === 200)
-      {
-        var response = JSON.parse(xhrUpdate.responseText);
+                        xhrUpdate.onload = function() {
+                            if(xhrUpdate.status === 200) {
+                                var response = JSON.parse(xhrUpdate.responseText);
 
-        if(response == 0)
-        {
-            users_table.innerHTML="";
-            getUsers();
-        }
-    }
-}
-   })
-//----------------------------------------------------------
-//----------------------------------------------------------
-btnDeactivate.addEventListener("click", function(event){
+                                if(response == 0) {
+                                    users_table.innerHTML="";
+                                    getUsers();
+                                }
+                            }
+                        }
+                    })
+                    //----------------------------------------------------------
 
-    const clickedButton = event.target;
-    var data = {
-        userid : clickedButton.dataset.userid
-     }
-    const xhrUpdate = new XMLHttpRequest();
-    xhrUpdate.open('POST', '/Approve/DeactivateUser', true);
-    xhrUpdate.setRequestHeader('Content-Type', 'application/json');
-    xhrUpdate.send(JSON.stringify(data));
+                    //----------------------------------------------------------
+                    btnDeactivate.addEventListener("click", function(event) {
+                        const clickedButton = event.target;
+                        var data = {
+                            userid : clickedButton.dataset.userid
+                        }
+
+                        const xhrUpdate = new XMLHttpRequest();
+                        xhrUpdate.open('POST', '/Approve/DeactivateUser', true);
+                        xhrUpdate.setRequestHeader('Content-Type', 'application/json');
+                        xhrUpdate.send(JSON.stringify(data));
    
-     xhrUpdate.onload = function(){
-        if(xhrUpdate.status === 200)
-      {
-        var response = JSON.parse(xhrUpdate.responseText);
+                        xhrUpdate.onload = function() {
+                            if(xhrUpdate.status === 200) {
+                                var response = JSON.parse(xhrUpdate.responseText);
 
-        if(response == 0)
-        {
-            users_table.innerHTML="";
-            getUsers();
-        }
-    }
-}
-   })
-//----------------------------------------------------------
-            c.appendChild(btnEdit);
-            c.appendChild(btnDelete);
-            c.appendChild(btnDeactivate);
-            row.appendChild(c);
-            users_table.appendChild(row);
+                                if(response == 0) {
+                                    users_table.innerHTML="";
+                                    getUsers();
+                                }
+                            }
+                        }
+                    })
+                    //----------------------------------------------------------
 
-
-    
-
+                    c.appendChild(btnEdit);
+                    c.appendChild(btnDelete);
+                    c.appendChild(btnDeactivate);
+                    row.appendChild(c);
+                    users_table.appendChild(row);
+                }
+            } else {
 
             }
-           
-
-        }
-        else
-        {
-
-        }
-
-        
-     
-
-        
-    
-    
-        }
-
-        
+        }   
     }
     xhrUsers.send();
-
-
-
-
 };
 
-
-
-
-var init = function(){
-
+var init = function() {
     getUsers();
-    
-  
-  
 };
-
 
 window.onload = init;
-
-
-                
-
-
