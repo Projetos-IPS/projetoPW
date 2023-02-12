7b03c76e49df70d0ec185bbf601f249ede7079fc
Joana Silva <101148531+JoanaSilvaHUS@users.noreply.github.com>
2023-02-12 15:51:55 +0000

    agora sim

--- 1F_PW_202100190_202100242/202100190_202100242/public/scripts/main.js
+++ 1F_PW_202100190_202100242/202100190_202100242/public/scripts/main.js
@@ -11,12 +11,12 @@ function getProfileData(){
     let menuApprove = document.getElementById('aprovar-utilizadores');
     let adminName = document.getElementById('admin-name-show');
     let teamMenu = document.getElementById('team-page');
-      
+    
         xhrUser.onload = function(){
             
             if(xhrUser.status === 200)
             {
-                let dataUser = JSON.parse(xhrUser.responseText);
+              let dataUser = JSON.parse(xhrUser.responseText);
     
                const xhrUserData = new XMLHttpRequest();
                xhrUserData.open('GET', '/Profile/getUserDataP', true);
@@ -25,17 +25,39 @@ function getProfileData(){
                 xhrUserData.onload = function(){
                     if(xhrUserData.status = 200){
     
-                        let UserData = JSON.parse(xhrUserData.responseText);
+                      let UserData = JSON.parse(xhrUserData.responseText);
+                        
+                      
+    
                         if(dataUser[0].tipo_utilizador == 'Profissional')
                         {
+                            if(UserData[0].genero == 'Feminino')
+                            {
+                                profileImg.src = "../images/profile-female.png";
+                                
+                            }
+                            else if(UserData[0].genero == 'Masculino')
+                            {
+                                profileImg.src = "../images/profile-male.png";
+                               
+                            }
+                            else if(UserData[0].genero == 'other')
+                            {
+                                profileImg.src = "../images/profile-other.png";
+                                
+                            }
+                    
                             menuApprove.style.display = "none";
                             menuPortfolios.style.display = "none";
                         }
                         if(dataUser[0].tipo_utilizador == 'Empresa')
                         {
                             menuJobOffers.style.display = "none";
+                            menuPortfolios.style.display = "none";
                             profileName.innerHTML = UserData[0].nome;
                             menuPortfolios.style.display = "none";
+                        
+                        
                         }
                         if(dataUser[0].tipo_utilizador == 'Admin')
                         {
@@ -45,7 +67,6 @@ function getProfileData(){
                             profileImg.style.display = "none";
                             adminName.style.display = "block";
                             teamMenu.style.display = "none";
-                            
                         }
     
     
@@ -54,6 +75,7 @@ function getProfileData(){
                 } 
     
                 xhrUserData.send();
+
              }
         }  
         xhrUser.send();
